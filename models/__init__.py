fca109647d98644c9c6a6212c225c439a820c100
Michael <michael@mshenoda.com>
2022-05-26 17:12:53 -0500

    Updated requirements

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 import imp
 from .yolo_tiny import YoloTiny
-from .yolo_resnet import YoloResNet
 from .yolo import Yolo
 from .create import create_model
--- utils/processing.py
+++ utils/processing.py
@@ -2,14 +2,12 @@ import numpy as np
 import torch
 from collections import Counter
 
-def encode(bboxs, S, B, num_classes):
-    # bboxs is a xywhc list: [(x,y,w,h,c),(x,y,w,h,c),....]
+def encode(bboxes, S, B, num_classes):
+    # bbox is cx, cy, width, height, classification
     label = np.zeros((S, S, 5 * B + num_classes))
-    for x, y, w, h, c in bboxs:
+    for x, y, w, h, c in bboxes:
         x_grid = int(x // (1.0 / S))
         y_grid = int(y // (1.0 / S))
-        # xx = x / (1.0 / S) - x_grid
-        # yy = y / (1.0 / S) - y_grid
         xx, yy = x, y
         label[y_grid, x_grid, 0:5] = np.array([xx, yy, w, h, 1])
         label[y_grid, x_grid, 5:10] = np.array([xx, yy, w, h, 1])
@@ -18,23 +16,23 @@ def encode(bboxs, S, B, num_classes):
 
 
 def decode(pred, S, B, num_classes, conf_thresh, iou_thresh):
-    bboxs = torch.zeros((S * S, 5 + num_classes))  # 49*25
+    bboxes = torch.zeros((S * S, 5 + num_classes))  # 49*25
     for x in range(S):
         for y in range(S):
             conf1, conf2 = pred[x, y, 4], pred[x, y, 9]
             if conf1 > conf2:
                 # bbox1
-                bboxs[(x * S + y), 0:4] = torch.Tensor([pred[x, y, 0], pred[x, y, 1], pred[x, y, 2], pred[x, y, 3]])
-                bboxs[(x * S + y), 4] = pred[x, y, 4]
-                bboxs[(x * S + y), 5:] = pred[x, y, 10:]
+                bboxes[(x * S + y), 0:4] = torch.Tensor([pred[x, y, 0], pred[x, y, 1], pred[x, y, 2], pred[x, y, 3]])
+                bboxes[(x * S + y), 4] = pred[x, y, 4]
+                bboxes[(x * S + y), 5:] = pred[x, y, 10:]
             else:
                 # bbox2
-                bboxs[(x * S + y), 0:4] = torch.Tensor([pred[x, y, 5], pred[x, y, 6], pred[x, y, 7], pred[x, y, 8]])
-                bboxs[(x * S + y), 4] = pred[x, y, 9]
-                bboxs[(x * S + y), 5:] = pred[x, y, 10:]
+                bboxes[(x * S + y), 0:4] = torch.Tensor([pred[x, y, 5], pred[x, y, 6], pred[x, y, 7], pred[x, y, 8]])
+                bboxes[(x * S + y), 4] = pred[x, y, 9]
+                bboxes[(x * S + y), 5:] = pred[x, y, 10:]
 
-    # apply NMS to all bboxs
-    xywhcc = nms(bboxs, num_classes, conf_thresh, iou_thresh)
+    # apply NMS to all bboxes
+    xywhcc = nms(bboxes, num_classes, conf_thresh, iou_thresh)
     return xywhcc
 
 def iou(bbox1, bbox2):
@@ -56,42 +54,43 @@ def iou(bbox1, bbox2):
         intersect = (min_right - max_left) * (min_bottom - max_top)
         return intersect / (area1 + area2 - intersect)
 
-def nms(bboxs, num_classes, conf_thresh=0.1, iou_thresh=0.3):
-    # Non-Maximum Suppression, bboxs is a 98*15 tensor
-    bbox_prob = bboxs[:, 5:].clone().detach()  # 98*10
-    bbox_conf = bboxs[:, 4].clone().detach().unsqueeze(1).expand_as(bbox_prob)  # 98*10
-    bbox_cls_spec_conf = bbox_conf * bbox_prob  # 98*10
+def nms(bboxes, num_classes, conf_thresh=0.1, iou_thresh=0.3):
+    # Non-Maximum Suppression
+
+    bbox_prob = bboxes[:, 5:].clone().detach()
+    bbox_conf = bboxes[:, 4].clone().detach().unsqueeze(1).expand_as(bbox_prob)
+    bbox_cls_spec_conf = bbox_conf * bbox_prob 
     bbox_cls_spec_conf[bbox_cls_spec_conf <= conf_thresh] = 0
 
-    # for each class, sort the cls-spec-conf score
+    # for each class, sort the class specific confidence
     for c in range(num_classes):
         rank = torch.sort(bbox_cls_spec_conf[:, c], descending=True).indices  # sort conf
         # for each bbox
-        for i in range(bboxs.shape[0]):
+        for i in range(bboxes.shape[0]):
             if bbox_cls_spec_conf[rank[i], c] == 0:
                 continue
-            for j in range(i + 1, bboxs.shape[0]):
+            for j in range(i + 1, bboxes.shape[0]):
                 if bbox_cls_spec_conf[rank[j], c] != 0:
-                    curr_iou = iou(bboxs[rank[i], 0:4], bboxs[rank[j], 0:4])
+                    curr_iou = iou(bboxes[rank[i], 0:4], bboxes[rank[j], 0:4])
                     if curr_iou > iou_thresh:
                         bbox_cls_spec_conf[rank[j], c] = 0
 
     # exclude cls-specific confidence score=0
-    bboxs = bboxs[torch.max(bbox_cls_spec_conf, dim=1).values > 0]
+    bboxes = bboxes[torch.max(bbox_cls_spec_conf, dim=1).values > 0]
 
     bbox_cls_spec_conf = bbox_cls_spec_conf[torch.max(bbox_cls_spec_conf, dim=1).values > 0]
 
-    ret = torch.ones((bboxs.size()[0], 6))
+    ret = torch.ones((bboxes.size()[0], 6))
 
     # return null
-    if bboxs.size()[0] == 0:
+    if bboxes.size()[0] == 0:
         return torch.tensor([])
 
     # bbox coord
-    ret[:, 0:4] = bboxs[:, 0:4]
+    ret[:, 0:4] = bboxes[:, 0:4]
     # bbox class-specific confidence scores
     ret[:, 4] = torch.max(bbox_cls_spec_conf, dim=1).values
     # bbox class
-    ret[:, 5] = torch.argmax(bboxs[:, 5:], dim=1).int()
+    ret[:, 5] = torch.argmax(bboxes[:, 5:], dim=1).int()
     return ret
 
